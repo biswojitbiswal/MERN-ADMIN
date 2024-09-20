67530c3d859d80ff258e85d587a3747e24cda174
Biswojit Biswal <biswojitb474@gmail.com>
2024-09-20 09:50:13 +0530

    changes in app component

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import ReactDOM from 'react-dom/client'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
@@ -14,34 +14,52 @@ import AdminLayout from './AdminLayout/AdminLayout'
 import AdminUser from './AdminLayout/AdminUser'
 import AdminContact from './AdminLayout/AdminContact'
 import AdminUpdate from './AdminLayout/AdminUpdate'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='' element={<Layout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='services' element={<Services />} />
-        <Route path='register' element={<Registration />} />
-        <Route path='login' element={<Login />} />
-        <Route path='logout' element={<Logout />} />
-        <Route path='admin' element={<AdminLayout />} >
-          <Route path='users' element={<AdminUser />} />
-          <Route path='contacts' element={<AdminContact />} />
-          <Route path='users/:id/edit' element={<AdminUpdate />} />
-        </Route>
-        <Route path='*' element={<Error />} />
-      </Route>
-    )
-  )
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path='' element={<Layout />}>
+  //       <Route path='/' element={<Home />} />
+  //       <Route path='about' element={<About />} />
+  //       <Route path='contact' element={<Contact />} />
+  //       <Route path='services' element={<Services />} />
+  //       <Route path='register' element={<Registration />} />
+  //       <Route path='login' element={<Login />} />
+  //       <Route path='logout' element={<Logout />} />
+  //       <Route path='admin' element={<AdminLayout />} >
+  //         <Route path='users' element={<AdminUser />} />
+  //         <Route path='contacts' element={<AdminContact />} />
+  //         <Route path='users/:id/edit' element={<AdminUpdate />} />
+  //       </Route>
+  //       <Route path='*' element={<Error />} />
+  //     </Route>
+  //   )
+  // )
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />} />
+  //       <Route path='about' element={<About />} />
+  //       <Route path='contact' element={<Contact />} />
+  //       <Route path='services' element={<Services />} />
+  //       <Route path='register' element={<Registration />} />
+  //       <Route path='login' element={<Login />} />
+  //       <Route path='logout' element={<Logout />} />
+  //       <Route path='admin' element={<AdminLayout />} >
+  //         <Route path='users' element={<AdminUser />} />
+  //         <Route path='contacts' element={<AdminContact />} />
+  //         <Route path='users/:id/edit' element={<AdminUpdate />} />
+  //       </Route>
+  //       <Route path='*' element={<Error />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
